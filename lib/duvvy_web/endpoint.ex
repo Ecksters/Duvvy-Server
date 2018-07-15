459dd7012f504b6a86f00caa880f2fa79c1649c0
Daniel Eck <Danny.C.eck@gmail.com>
2018-07-14 17:42:20 -0700

    Enable CORS for local client access
    Enabled CORSPlug to allow the dev server for the client to connect to the API server even though both are hosted on different ports.

--- lib/duvvy_web/endpoint.ex
+++ lib/duvvy_web/endpoint.ex
@@ -3,14 +3,6 @@ defmodule DuvvyWeb.Endpoint do
 
   socket "/socket", DuvvyWeb.UserSocket
 
-  # Serve at "/" the static files from "priv/static" directory.
-  #
-  # You should set gzip to true if you are running phoenix.digest
-  # when deploying your static files in production.
-  plug Plug.Static,
-    at: "/", from: :duvvy, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
-
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
@@ -29,13 +21,7 @@ defmodule DuvvyWeb.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
 
-  # The session will be stored in the cookie and signed,
-  # this means its contents can be read but not tampered with.
-  # Set :encryption_salt if you would also like to encrypt it.
-  plug Plug.Session,
-    store: :cookie,
-    key: "_duvvy_key",
-    signing_salt: "qrP5l40r"
+  plug CORSPlug, origin: ["http://localhost.com:3000", "http://127.0.0.1:3000"]
 
   plug DuvvyWeb.Router
 
